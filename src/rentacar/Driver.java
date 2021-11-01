b4b5253d46d135b7dfa563d8b73f55babd6cd9a3
danielmartellini <danielbmartellini@gmail.com>
2021-11-01 20:04:00 +0000

    v1
    Created new classes and implemented the interfaces

--- src/rentacar/Driver.java
+++ src/rentacar/Driver.java
@@ -16,11 +16,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// INCLUDE HERE THE LINK TO THE GIT REPOSITORY AS A COMMENT!!!!
+// https://github.com/danielmartellini/RentACar
 
 
-// INCLUDE HERE THE FULL NAME AND STUDENT NUMBER OF BOTH TEAM MEMBERS 
-// AS A COMMENT!!!!
+// DANIEL BEZERRA MARTELLINI 
+// 2020356
 
 
 
